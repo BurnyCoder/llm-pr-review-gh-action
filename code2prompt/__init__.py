268ac717a883a2ca2c9018247e8282c94e14192a
BurnyCoder <happymancz@email.cz>
2025-03-17 13:28:06 +0100

    made code2prompt a module

---
+++ code2prompt/__init__.py
@@ -0,0 +1,8 @@
+"""
+code2prompt package for analyzing codebases and generating prompts.
+"""
+
+# Import the get_codebase function to make it available at the package level
+from .get_codebase import get_codebase
+
+__all__ = ['get_codebase'] 
